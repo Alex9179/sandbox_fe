7c8f9c9fa9acec413a80f04127c1ebe17b273674
Alex <alexrmclean91@gmail.com>
2023-05-03 09:51:46 +0100

    fe round 4.

---
+++ src/components/BuildingPolys.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-btn elevation="2" @click="buttonClick"> Load</v-btn>
+    <GmapMap
+      ref="mapRef"
+      :center="center"
+      :zoom="zoom"
+      :options="mapOptions"
+      :style="mapStyles"
+    />
+  </div>
+</template>
+
+<script>
+//import axios from "axios";
+
+export default {
+  name: "BuildingPolys",
+  data: () => ({
+    zoom: 13,
+    center: { lat: 50.82858299245613, lng: -0.13796688117682637 },
+    data_layer: null,
+    mapOptions: {
+      gestureHandling: "greedy",
+    },
+    mapStyles: {
+      width: "100%",
+      height: "93.9vh",
+    },
+  }),
+  mounted() {
+    this.mapSetup();
+  },
+  methods: {
+    buttonClick() {
+      console.log("Clicked.");
+    },
+    mapSetup() {
+      // set up the layers
+      //this.$refs.mapRef.$mapPromise.then((map) => {
+        //this.data_layer = new google.maps.Data({ map: map });
+      //});
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_API_KEY,
+    libraries: 'drawing, places, geolocation, geometry',
+  },
+  installComponents: true
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  VueGoogleMaps,
+  render: (h) => h(App),
+}).$mount("#app");
